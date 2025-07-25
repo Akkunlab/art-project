15fa9069d8ba1f20a75a6627dc23cb94ba1f447f
Akkun <38970950+Akkunlab@users.noreply.github.com>
2025-07-25 19:52:18 +0900

    feat: refactor value change handling and update button label table with additional controls

--- dmx-controller/src/automation.py
+++ dmx-controller/src/automation.py
@@ -1,25 +1,23 @@
 # me - this DAT
-# 
 # channel - the Channel object which has changed
 # sampleIndex - the index of the changed sample
 # val - the numeric value of the changed sample
 # prev - the previous sample value
-# 
 # Make sure the corresponding toggle is enabled in the CHOP Execute DAT.
 
-def onOffToOn(channel, sampleIndex, val, prev):
-	return
-
-def whileOn(channel, sampleIndex, val, prev):
-	return
-
-def onOnToOff(channel, sampleIndex, val, prev):
-	return
-
-def whileOff(channel, sampleIndex, val, prev):
-	return
+sliders = [
+    'slider1',
+    'slider7',
+    'slider20',
+    'slider32',
+    'slider37',
+]
 
 def onValueChange(channel, sampleIndex, val, prev):
-	op('/project1/GUI/slider32/field1/string')[0, 0] = round(val)
-	return
-	
+    button_val = op('/project1/side_bar/null1')['v5']
+
+    if button_val:
+        for i in range(len(sliders)):
+            op(f'/project1/GUI/{sliders[i]}/field1/string')[0, 0] = round(val)
+
+    return
